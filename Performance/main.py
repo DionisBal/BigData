b59c4581fc6f426650162e2fc9c5b1302fa41b02
DionisBal <94252667+DionisBal@users.noreply.github.com>
2024-01-30 20:56:11 +0200

    Add files via upload

--- Performance/main.py
+++ Performance/main.py
@@ -1,6 +1,7 @@
 import sys
 import psutil
 import time
+import matplotlib.pyplot as plt
 
 
 def memory_usage(pid):
@@ -9,10 +10,9 @@ def memory_usage(pid):
     return mem
 
 
-def ram_usage(pid):
+def vms_usage(pid):
     process = psutil.Process(pid)
-    ram_percentage = process.memory_info()[1] / float(2 ** 20)
-    ram_percentage = ram_percentage / psutil.virtual_memory().total
+    ram_percentage = process.memory_percent(memtype='vms')
     return ram_percentage
 
 
@@ -39,27 +39,71 @@ def get_port_pid(port):
 if __name__ == '__main__':
     start_time = time.time()
     arguments = sys.argv[1:]
-    if arguments[0] != "-ports" or len(arguments) == 1:
-        raise Exception("Please specify at least one port number")
-    cpu_matrix = [[]] * (len(arguments) - 1)
-    mem_matrix = [[]] * (len(arguments) - 1)
-    ram_matrix = [[]] * (len(arguments) - 1)
+    if (arguments[0] != "-ports" and arguments[0] != "-pid") or len(arguments) == 1:
+        raise Exception("Please specify at least one port or pid number")
+    cpu_matrix = []
+    mem_matrix = []
+    ram_matrix = []
+    print(arguments)
     while True:
+        max_rss = 0
+        max_vms = 0
+        max_cpu = 0
         for p in range(len(arguments[1:])):
-            port = arguments[p + 1]
-            # pid = get_port_pid(int(port))
-            pid = int(port)
-            if psutil.pid_exists(pid):
-                if cpu_usage(pid) > 0.01:
-                    cpu_matrix[p].append((cpu_usage(pid), time.time() - start_time))
-                    print(cpu_usage(pid))
-                if memory_usage(pid) > 0.0001:
-                    mem_matrix[p].append((memory_usage(pid), time.time() - start_time))
-                    print(memory_usage(pid))
-
-                ram_matrix[p].append((ram_usage(pid), time.time() - start_time))
-                print(ram_usage(pid))
+            if arguments[0] == '-ports':
+                port = arguments[p + 1]
+                pid = get_port_pid(int(port))
             else:
-                print(cpu_matrix)
+                pid = int(arguments[p+1])
+            # pid = int(port)
+            try:
+                if cpu_usage(pid) / psutil.cpu_count() > max_cpu:
+                    max_cpu = cpu_usage(pid) / psutil.cpu_count()
+
+                if memory_usage(pid) > max_rss:
+                    max_rss = memory_usage(pid)
+                # print(memory_usage(pid))
+
+                if vms_usage(pid) > max_vms:
+                    max_vms = vms_usage(pid)
+                # print(ram_usage(pid))
+
+            except Exception:
+                x = []
+                y = []
+
+                for tup in mem_matrix:
+                    y.append(tup[0])
+                    x.append(tup[1])
+
+                plt.plot(x, y)
+                plt.title("% RSS Usage")
+                plt.show()
+
+                x = []
+                y = []
+
+                for tup in cpu_matrix:
+                    y.append(tup[0])
+                    x.append(tup[1])
+
+                plt.plot(x, y)
+                plt.title("% CPU Usage")
+                plt.show()
+
+                x = []
+                y = []
+                for tup in ram_matrix:
+                    y.append(tup[0])
+                    x.append(tup[1])
+
+                plt.plot(x, y)
+                plt.title("% VMS Usage")
+
+                plt.show()
+                # plt.savefig(r'C:\Users\mdbal\Desktop\Bid_Data_Plots\ray_loop_4.png')
                 exit(0)
+        cpu_matrix.append((max_cpu, time.time()-start_time))
+        mem_matrix.append((max_rss, time.time() - start_time))
+        ram_matrix.append((max_vms, time.time() - start_time))
         time.sleep(0.1)
